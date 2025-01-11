4de888474b1eaf3eb4de5025f3e5b6db068319ed
Ilyas73-hue <ilyasfullstackdeveloper@gmail.com>
2025-01-11 15:52:04 +0530

    all updated

--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import "./Menu.css";
 import { IoMdSearch } from "react-icons/io";
 import food_icon from "../../RoyalAssets/food_icon.png";
-import food_img_1 from "../../RoyalAssets/Fried Chicken.png";
 import axios from "axios";
  
 function Menu() {
 
-    const [categoriesdata , setCategoriesdata] = useState("Desi breakfast");
+    const [categoriesdata , setCategoriesdata] = useState("Desi Breakfast");
     const [filtermenu, setFiltermenu] = useState([])
 
     // console.log(categoriesdata);
 
     const [carddata, setCarddata] = useState([]);
+
+    const [cardid , setCardId] = useState();
 
     useEffect(() => {
    const data =   axios.get(`http://localhost:1000/api/menu/get`)
@@ -23,10 +24,51 @@ function Menu() {
 
  const Filter = (event) => {
   setCategoriesdata(event)
-    setFiltermenu(carddata.filter(f => f.categories.toLowerCase().includes(event)))
+    setFiltermenu(carddata.filter(f => f.categories.toLowerCase().includes(event.toLowerCase())))
  }
 
- console.log(filtermenu)
+
+
+ const [onedata, setOneData] = useState();
+
+
+
+ const cartget = async(id) => {
+  
+  const data =  await axios.get(`http://localhost:1000/api/menu/get/${id}`)
+
+  setTimeout(console.log("data"), 6000)
+
+
+  console.log("data")
+
+  // setOneData(data.data.menu)
+
+
+  // setTimeout(axios.post(`http://localhost:1000/api/cart/post`, {
+  //   name: onedata.name, price: onedata.price,  img: onedata.img,categories: onedata.categories, description: onedata.description, adminId: onedata.adminId, userId: localStorage.getItem("userId")
+  // }) , 5000)
+
+
+ }
+
+//  const cartpost = async(id) => {
+
+//   const post = await axios.post(`http://localhost:1000/api/cart/post`, {
+//     name: onedata.name, price: onedata.price,  img: onedata.img,categories: onedata.categories, description: onedata.description, adminId: onedata.adminId, userId: localStorage.getItem("userId")
+//   })
+
+
+//   setTimeout(post, 3000)
+
+//   console.log(post)
+ 
+//  }
+
+//  console.log(onedata);
+
+
+
 
   return (
     <div id="menu">
@@ -70,15 +112,15 @@ function Menu() {
 
              </div>
              <div id="menu-section-2-div-2-1-2-1">
-               <div id="menu-section-2-div-2-1-2-1-1" onClick={() => Filter("desi breakfast")}>
+               <div id="menu-section-2-div-2-1-2-1-1" onClick={() => Filter("Desi Breakfast")}>
                  <img  src={food_icon} alt={food_icon} />
-                 <p id={ categoriesdata === "Desi breakfast" ? "menu-section-2-div-2-1-2-1-1-p-1-active" : "menu-section-2-div-2-1-2-1-1-p-1"}>Desi Breakfast</p>
+                 <p id={ categoriesdata.toLowerCase() === "desi breakfast" ? "menu-section-2-div-2-1-2-1-1-p-1-active" : "menu-section-2-div-2-1-2-1-1-p-1"}>Desi Breakfast</p>
                </div>
              </div>
-             <div id="menu-section-2-div-2-1-2-1" onClick={() => Filter("special non veg dishes") }>
+             <div id="menu-section-2-div-2-1-2-1" onClick={() => Filter("Special Non Veg Dishes") }>
                <div id="menu-section-2-div-2-1-2-1-1">
                  <img  src={food_icon} alt={food_icon} />
-                 <p id={ categoriesdata === "special non veg dishes" ? "menu-section-2-div-2-1-2-1-1-p-1-active" : "menu-section-2-div-2-1-2-1-1-p-1"}>Special Non Veg Dishes</p>
+                 <p id={ categoriesdata.toLowerCase() === "special non veg dishes" ? "menu-section-2-div-2-1-2-1-1-p-1-active" : "menu-section-2-div-2-1-2-1-1-p-1"}>Special Non Veg Dishes</p>
                </div>
              </div>
            </div>
@@ -93,53 +135,29 @@ function Menu() {
             <div className='container' id="menu-section-2-div-2-2-1">
                  <p id="menu-section-2-div-2-2-1-p-1">{categoriesdata}</p>
                  <div id="menu-section-2-div-2-2-1-card">
-                 <div id="menu-section-2-div-2-2-1-card-1">
-                     <div id="menu-section-2-div-2-2-1-card-1-1">
-                       <img id="menu-section-2-div-2-2-1-card-1-1-img" src={food_img_1} alt={food_img_1} />
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-2">
-                       <p id="menu-section-2-div-2-2-1-card-1-2-p-1">chicken parotta</p>
-                       <p id="menu-section-2-div-2-2-1-card-1-2-p-2">$ 9</p>
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-3">
-                      <p id="menu-section-2-div-2-2-1-card-1-3-p-1">Made with eggs, lettuce, salt, oil and other ingredients.</p>
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-4">
-                        <button id="menu-section-2-div-2-2-1-card-1-4-button">Order Now</button>
-                     </div>
-                 </div>
 
-                 <div id="menu-section-2-div-2-2-1-card-1">
-                     <div id="menu-section-2-div-2-2-1-card-1-1">
-                       <img id="menu-section-2-div-2-2-1-card-1-1-img" src={food_img_1} alt={food_img_1} />
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-2">
-                       <p id="menu-section-2-div-2-2-1-card-1-2-p-1">chicken parotta</p>
-                       <p id="menu-section-2-div-2-2-1-card-1-2-p-2">$ 9</p>
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-3">
-                      <p id="menu-section-2-div-2-2-1-card-1-3-p-1">Made with eggs, lettuce, salt, oil and other ingredients.</p>
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-4">
-                        <button id="menu-section-2-div-2-2-1-card-1-4-button">Order Now</button>
-                     </div>
-                 </div>
+{
+  filtermenu.map((item) => (
+    <div id="menu-section-2-div-2-2-1-card-1" key={item._id}>
+    <div id="menu-section-2-div-2-2-1-card-1-1">
+      <img id="menu-section-2-div-2-2-1-card-1-1-img" src={item.img} alt={item.img} />
+    </div>
+    <div id="menu-section-2-div-2-2-1-card-1-2">
+      <p id="menu-section-2-div-2-2-1-card-1-2-p-1">{item.name}</p>
+      <p id="menu-section-2-div-2-2-1-card-1-2-p-2">$ {item.price}</p>
+    </div>
+    <div id="menu-section-2-div-2-2-1-card-1-3">
+     <p id="menu-section-2-div-2-2-1-card-1-3-p-1">{item.description}</p>
+    </div>
+    <div id="menu-section-2-div-2-2-1-card-1-4">
+       <button id="menu-section-2-div-2-2-1-card-1-4-button" onClick={() => { cartget(`${item._id}`) }}>Order Now</button>
+    </div>
+</div>
+  ))
+}
 
-                 <div id="menu-section-2-div-2-2-1-card-1">
-                     <div id="menu-section-2-div-2-2-1-card-1-1">
-                       <img id="menu-section-2-div-2-2-1-card-1-1-img" src={food_img_1} alt={food_img_1} />
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-2">
-                       <p id="menu-section-2-div-2-2-1-card-1-2-p-1">chicken parotta</p>
-                       <p id="menu-section-2-div-2-2-1-card-1-2-p-2">$ 9</p>
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-3">
-                      <p id="menu-section-2-div-2-2-1-card-1-3-p-1">Made with eggs, lettuce, salt, oil and other ingredients.</p>
-                     </div>
-                     <div id="menu-section-2-div-2-2-1-card-1-4">
-                        <button id="menu-section-2-div-2-2-1-card-1-4-button">Order Now</button>
-                     </div>
-                 </div>
+               
+
 
 
                  </div>
